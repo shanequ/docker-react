cccc6746c5b08c82e4eaec754f7247beb8d79fae
Xun Qu <shanequ@hotmail.com>
2020-02-15 11:26:54 +1100

    change some text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello! This is the dev server. I am happy!
+          Hello! This is the prod server in AWS!
         </p>
         <a
           className="App-link"
